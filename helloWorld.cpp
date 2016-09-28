3d063f926a36ea5951f5c74ac4038b5d5f0f7c8e
Daniel Freire <dfgs.fish@gmail.com>
2016-09-28 20:33:47 -0300

    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+/**
+ * This file shows a simple Hello World using
+ * C++ language.
+ * @author: Daniel Freire
+ * @date: 03/16/2007
+ */
+ 
+ #include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "My first Hello World on C++ language! :D" << endl;
+	
+	return 0;
+}
