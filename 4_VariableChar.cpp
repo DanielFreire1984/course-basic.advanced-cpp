b061a39c21e3946073f8e52773d7fd8c460c1322
Daniel Freire <dfgs.fish@gmail.com>
2016-09-29 19:13:58 -0300

    Add files via upload

---
+++ 4_VariableChar.cpp
@@ -0,0 +1,25 @@
+/**
+ * This file shows use for character assignment
+ * C++ language.
+ * @author: Daniel Freire
+ * @date: 03/16/2007
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	char c1, c2, c3, c4, c5, c6;
+	c1 = 'D';
+	c2 = 'a';
+	c3 = 'n';
+	c4 = 'i';
+	c5 = 'e';
+	c6 = 'l';
+	cout << "Simple code to printing character variables\n" << endl;
+	cout << "Printing name: " << c1 << c2 << c3 << c4 << c5 << c6 << endl;
+	
+	return 0;
+}
