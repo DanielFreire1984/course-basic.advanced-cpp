15906ae7d25f35655da82f1c575ef30568ceebc6
Daniel Freire <dfgs.fish@gmail.com>
2016-09-29 23:17:49 -0300

    Add files via upload

---
+++ 10_overloadFunctions.cpp
@@ -0,0 +1,38 @@
+/**
+ * This example code shows how to use
+ * overload functions.
+ * C++ language.
+ * 
+ * @author: Daniel Freire
+ * @date: 03/16/2007
+ * 
+ * Note: The 3 function called add
+ * has diferent parameters
+ */
+
+#include <iostream>
+
+using namespace std;
+//Function with two parameters and same types
+void add(int n1, int n2){
+	cout << n1 + n2 <<endl;
+}
+//Function with three parameters and same types
+void add(int n1, int n2, int n3){
+	cout << n1 + n2 +n3 << endl;
+}
+//Function with two parameters and diferent types
+void add(int n1, double n2){
+	cout << n1 + n2 << endl;
+}
+
+int main(int argc, char** argv)
+{
+	//calling function with same name but diferent parameters
+	add(1,2);
+	add(1,2,3);
+	add(1, 2.3);	
+	return 0;
+}
+
+
