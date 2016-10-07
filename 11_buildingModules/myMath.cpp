1e5126e4df6bdcf24f9f56efd4b26b970644200f
Daniel Freire <dfgs.fish@gmail.com>
2016-10-06 23:08:38 -0300

    Create myMath.cpp

---
+++ 11_buildingModules/myMath.cpp
@@ -0,0 +1,17 @@
+//Function to calculate the square area
+float areaQuad(float side)
+{
+	return side * side;
+}
+
+//Function to calculate the triangle rectangle area
+float areaTrianguleEqui(float base, float height)
+{
+	return (base * height) /2;
+}
+
+//Function to calculate rectangle area
+float areaRectangule(float base, float height)
+{
+	return base * height;
+}
